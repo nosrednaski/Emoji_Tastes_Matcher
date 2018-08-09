5c8f3a2b76b94e8ceb6430cbb2a2f9f1200702fe
Unknown <nosrednaski@gmail.com>
2018-08-09 08:49:00 -0400

    Added Routes, HTML and Server
    Struggling with calculating total difference.

---
+++ app/data/friendsdata.js
@@ -0,0 +1,12 @@
+var friendArray = [
+    {
+    "name":"Hamster Hands",
+    "photo":"https://meggardiner.files.wordpress.com/2008/10/ham460.jpg",
+    "scores":[5,1,4,4,5,1,2,5,4,1]
+  },
+  {
+    "name":"Halitosis Jones",
+    "photo":"https://imgur.com/gallery/4fFxaWd",
+    "scores":[2,2,2,2,2,2,2,2,2,5]
+  },
+]
