d2864d45f094bf7f2db0f21eb2f4bc79503772f8
Unknown <nosrednaski@gmail.com>
2018-08-14 17:24:08 -0400

    Compatability Logic in API routing
    1. Can't figure out how to get css to display.
    2. compatibility Modal not functioning

--- app/data/friendsdata.js
+++ app/data/friendsdata.js
@@ -1,12 +1,14 @@
 var friendArray = [
-    {
+  {
     "name":"Hamster Hands",
-    "photo":"https://meggardiner.files.wordpress.com/2008/10/ham460.jpg",
     "scores":[5,1,4,4,5,1,2,5,4,1]
   },
   {
     "name":"Halitosis Jones",
-    "photo":"https://imgur.com/gallery/4fFxaWd",
     "scores":[2,2,2,2,2,2,2,2,2,5]
   },
+  {
+    "name":"Humbolt Humbolt",
+    "scores":[1,2,3,4,5,4,3,2,1,2]
+  }
 ]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,64 +1,41 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
+
 
 const friendsData = require("../data/friendsdata");
 
-
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
+  
   app.get("/api/friends", function(req, res) {
     res.json(friendsData);
   });
 
- 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
-
   app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body-parser middleware
+      //  compatability logic
+      const newUser = req.body;
+      const diff = (i) => { 
+        return newUser.scores
+        .map((x, j) => Math.abs(x - friendsData[i].scores[j]))
+        .map(Number)
+        .reduce((acc,val) => acc + val);
+    };
     
-      friendsData.push(req.body);
-      res.json(true);
+    const findMatch = () => {
+        let diffArr = [];
+        for(let i = 0; i < friendsData.length; i++) {
+            diffArr.push(diff(i));
+        };
+        let minIndex = diffArr.indexOf(Math.min(...diffArr));
+        const modalName = friendsData[minIndex].name;
+        console.log(`Index of Minimum: ${minIndex}\nThe Name to Modal: ${modalName}`);
+        friendsData.push(newUser);
+        console.log(`New array: ${friendsData}`); // <-------why does it show [object, Object]?
+        console.log(friendsData);
+    }
+    findMatch();
 
-      let mapThis = (x, j) => [Math.abs(x - scoreA[4].scores[j])]
-
-      let totalDifference = user
-          .map(mapThis)
-          .map(Number)
-          .reduce((acc,val) => acc + val);
-      console.log(totalDifference)
-    });
+  });
     
-  
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
   app.post("/api/clear", function() {
-    // Empty out the arrays of data
-    friendsData = [];
-
+       friendsData = [];
     console.log(friendsData);
   });
 };
